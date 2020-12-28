895bf48a081c6ec0380035459b1f6f086039cc54
roshan_piumal <rpiumal@mitrai.com>
2020-12-28 23:25:41 +0530

    added checkbox

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import './index.css';
 
 function App() {
   const [status, setStatus] = useState("Not Delivered");
+  const [checked, setChecked] = useState(false);
   return (
     <div className="App">
       <h1>The package is: {status}</h1>
       <button onClick={() => setStatus("Delivered")}>Deliver</button>
+      <input type="checkbox" value={checked} onClick={() => setChecked(!checked)}></input>
+      <p>checked: {checked ? "checked": "unchecked"}</p>
     </div>
   );
 }
